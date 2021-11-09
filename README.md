a40a692c529cb6455a957c791bd894a3bc74e4a9
Naiyoma <lankas.aurelia@gmail.com>
2021-11-09 23:07:03 +0300

    token farm contract

---
+++ README.md
@@ -0,0 +1,8 @@
+# _{Token Farm}_
+
+## Users Should be Able to:
+
+* _StakeTokens_
+* _unStakeTokens_
+* _issueTokens_
+* _GetETHValue_
--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,9 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DappToken is ERC20 {
     constructor() public ERC20("Dapp Token", "DAPP"){
-        _mint(msg.sender, 1000000000000000000)
+        _mint(msg.sender, 1000000000000000000);
     }
 }
