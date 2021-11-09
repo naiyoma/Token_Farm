a0f28bd64b2dc2f3d54fed5a1395adcbfc7e94d6
Naiyoma <lankas.aurelia@gmail.com>
2021-11-09 22:58:30 +0300

    Dapp token contract

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,3 +1,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DappToken is ERC20 {
+    constructor() public ERC20("Dapp Token", "DAPP"){
+        _mint(msg.sender, 1000000000000000000)
+    }
+}
