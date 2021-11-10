dbc2ef55cd19d05c56b3bab10e78497e7342573d
Naiyoma <lankas.aurelia@gmail.com>
2021-11-10 22:31:21 +0300

    issue tokens

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -3,13 +3,40 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract TokenFarm is Ownable{
 
+    mapping(address => mapping(address =>uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokenStaked;
+    address[] public stakers;
     address[] public allowedTokens;
+    IERC20 public dappToken;
+
+    constructor(address _dappTokenAddress) public {
+        dappToken = IERC20(_dappTokenAddress);
+    }
+
+    function issueToken() public onlyOwner {
+        for (
+            uint256 stakersIndex = 0;
+            stakersIndex < stakers.length;
+            stakersIndex++
+        ){
+            address recipient = stakers[stakersIndex];
+        }
+    }
 
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "Amount must be more than 0");
+        require(tokenIsAllowed(_token), "TOoken is not allowed");
+        IERC20(-token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokenStaked(msg.sender, _token);
+        stakingBalance[_token][msg.sender] = stakingBalance[_token][msg.sender] + _amount;
+        if (uniqueTokenStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
     }
 
     function addAllowedTokens(address _token) public onlyOwner{
